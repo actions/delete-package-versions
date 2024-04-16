cb50479ab2483c588078d667f75c0e3afd5b1a70
ManiMatter <124743318+ManiMatter@users.noreply.github.com>
2024-04-16 19:08:04 +0200

    Update get-versions.ts

--- src/version/get-versions.ts
+++ src/version/get-versions.ts
@@ -70,7 +70,16 @@ export function getOldestVersions(
             id: version.id,
             version: version.name,
             created_at: version.created_at,
-            tagged
+            tagged,
+            subIds: [] <<<<<<<<<<<<<<<<<<<<<<<<<<<<< First, we need to get for all packages the subIds (if they have any)
+              Example: https://github.com/ManiMatter/decluttarr/pkgs/container/decluttarr/204442395?tag=v1.38.0
+              Package v1.38.0: sha256:b4a9b04d8c0a5ab9f400f7f64f8be20d9951a996fd00882a936087af8f5ce43d
+              Has 3 Sub-IDs:
+                linux/amd64:      sha256:c2dfb515fd9a6ad396fe6a48cd3e535b4079b467cb691bcb3faede6889089d6e
+                linux/arm64:      sha256:59b2aa2e04cc6b3391f612833e87bbd0c4fdfddb04845b8e8f0365a45e90151c
+                unknown/unknown:  sha256:6dfc07ab69cbe95303f51fed14b40a9574bbebbb3501d7aec481d184a8321c91   
+
+
           }
         }),
         page,
